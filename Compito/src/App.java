202150d225add84d26cab02d0a9959b2b81ea7a4
lai2-01 <lai2-01@itismeucci.com>
2023-05-25 14:12:38 +0200

    aggiunta Jasir

--- Compito/src/App.java
+++ Compito/src/App.java
@@ -4,10 +4,13 @@ public class App {
         Box box = new Box();
         Monoposto m1 = new Monoposto("fiat", "001", "cioimpo", semaforo, box);
         Monoposto m2 = new Monoposto("ferrari", "002", "chinga", semaforo, box);
+        Monoposto m3 = new Monoposto("Ducati", "003", "Jasir", semaforo, box);
         m2.start();
         m1.start();
+        m3.start();
         m1.join();
         m2.join();
+        m3.join();
         System.out.println("---- GARA TERMINATA ----");
     }
 }
